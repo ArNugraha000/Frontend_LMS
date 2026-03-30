53018cd08817e18290d4f20aa216ffd427bcdc00
ArNugraha <Agestyofinding@gmail.com>
2026-03-30 16:31:57 +0700

    s

--- FE_LMS/src/app/pages/configuration/configuration.component.ts
+++ FE_LMS/src/app/pages/configuration/configuration.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from "@angular/core";
 import { DivisiService } from "app/Service/divisi.service";
 import { DepartemenService } from "app/Service/departemen.service";
 import { SeksieService } from "app/Service/seksie.service";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { ViewChild, TemplateRef } from "@angular/core";
+import { CookieService } from "ngx-cookie-service";
 
 @Component({
   selector: "app-master",
@@ -18,7 +21,17 @@ export class ConfigurationComponent implements OnInit {
     private divisiService: DivisiService,
     private departemenService: DepartemenService,
     private seksieService: SeksieService,
+    private modalService: NgbModal,
+    private cookiesService: CookieService,
   ) {}
+
+  @ViewChild("modalDivisi") modalDivisi!: TemplateRef<any>;
+  @ViewChild("modalDepartemen") modalDepartemen!: TemplateRef<any>;
+  @ViewChild("modalSeksi") modalSeksi!: TemplateRef<any>;
+
+  formDivisi: any = {};
+  formDepartemen: any = {};
+  formSeksi: any = {};
 
   ngOnInit() {
     this.loadDivisi();
@@ -53,9 +66,21 @@ export class ConfigurationComponent implements OnInit {
     });
   }
 
-  // 🔥 ACTION
   tambah(type: string) {
-    console.log("Tambah:", type);
+    if (type === "divisi") {
+      this.formDivisi = {};
+      this.modalService.open(this.modalDivisi, { size: "md" });
+    }
+
+    if (type === "departemen") {
+      this.formDepartemen = {};
+      this.modalService.open(this.modalDepartemen, { size: "md" });
+    }
+
+    if (type === "seksi") {
+      this.formSeksi = {};
+      this.modalService.open(this.modalSeksi, { size: "md" });
+    }
   }
 
   edit(item: any) {
@@ -64,5 +89,64 @@ export class ConfigurationComponent implements OnInit {
 
   delete(item: any) {
     console.log("Delete:", item);
+  }
+
+  saveDivisi(modal: any) {
+    const userName = this.cookiesService.get("userName") || "SYSTEM";
+
+    this.formDivisi.divStatus = "0";
+    this.formDivisi.divCreateBy = userName;
+    this.divisiService.save(this.formDivisi).subscribe(() => {
+      this.loadDivisi();
+      modal.close();
+    });
+  }
+
+  saveDepartemen(modal: any) {
+    const userName = this.cookiesService.get("userName") || "SYSTEM";
+
+    const payload = {
+      depJudul: this.formDepartemen.depJudul,
+      depNama: this.formDepartemen.depNama,
+      depStatus: "0",
+      depCreateBy: userName, // ✅ TAMBAHAN DI SINI
+      divisi: {
+        divId: this.formDepartemen.divId,
+      },
+    };
+
+    console.log("PAYLOAD:", payload);
+
+    this.departemenService.save(payload).subscribe({
+      next: (res) => {
+        console.log("SUCCESS", res);
+        this.loadDepartemen();
+        modal.close();
+      },
+      error: (err) => {
+        console.error("ERROR", err);
+      },
+    });
+  }
+
+  saveSeksi(modal: any) {
+    const userName = this.cookiesService.get("userName") || "SYSTEM";
+
+    const payload = {
+      sekJudul: this.formSeksi.sekJudul,
+      sekNama: this.formSeksi.sekNama,
+      sekStatus: "0",
+      sekCreateBy: userName,
+      departemen: {
+        depId: this.formSeksi.depId, // ✅ FIX DI SINI
+      },
+    };
+
+    console.log("PAYLOAD SEKSI:", payload);
+
+    this.seksieService.save(payload).subscribe(() => {
+      this.loadSeksie();
+      modal.close();
+    });
   }
 }
