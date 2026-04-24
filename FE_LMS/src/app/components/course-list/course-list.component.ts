c60750f223c6c0fc11cab57f534d7a163acb1176
ArNugraha <Agestyofinding@gmail.com>
2026-04-24 13:49:26 +0700

    pushcourse

--- FE_LMS/src/app/components/course-list/course-list.component.ts
+++ FE_LMS/src/app/components/course-list/course-list.component.ts
@@ -5,6 +5,7 @@ import { Kursus } from "../../models/kursus.model";
 @Component({
   selector: "app-course-list",
   templateUrl: "./course-list.component.html",
+  styleUrls: ["./course-list.component.css"],
 })
 export class CourseListComponent implements OnInit {
   allData: Kursus[] = [];
